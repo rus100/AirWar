f6aae148cb916e3b41cc273837397208045f9ccd
rus100 <rus100@yandex.ru>
2016-06-07 21:17:12 +0500

    Create Airplane.cs

---
+++ Airplane.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AirWar
+{
+    class Airplane
+    {
+       public  float life=0;
+      public  float fuel=0;
+       public float speed=0;
+       public int score = 0;
+       public float y = 0;
+    }
+}
